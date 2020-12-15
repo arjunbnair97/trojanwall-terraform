d0796831cbd05cac2476a556afb4df2c25323563
arjunbnair97 <53293413+arjunbnair97@users.noreply.github.com>
2020-12-16 03:42:14 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -23,3 +23,9 @@
     sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\\PasswordAuthentication yes" /etc/ssh/sshd_config
     sudo chown centos:centos /home/ansadmin
     sudo chmod 0700 /home/ansadmin
+    cd /home
+    ssh-keygen -t rsa -f /home/centos/.ssh/id_rsa -q -P ""
+    cd /home/centos/.ssh
+    cp /home/ansadmin/ansible-workdir/ssh-key-auto/* ./
+    sudo chmod +x ssh-key.sh
+    ./ssh-key.sh
