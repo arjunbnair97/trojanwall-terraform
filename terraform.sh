7bebdce45c3b6cb51fed31f52c3cbe9c08929974
arjunbnair97 <53293413+arjunbnair97@users.noreply.github.com>
2021-01-23 02:08:35 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -29,4 +29,5 @@
     cd /home/centos/.ssh
     cp /home/ansadmin/ansible-workdir/ssh-key-auto/* ./
     sudo chmod +x ssh-key.sh
+    sshpass -f passd.txt ssh-copy-id -i ~/.ssh/id_rsa.pub centos@trojanwall.southeastasia.cloudapp.azure.com
     ./ssh-key.sh
